43d2ef38f9feaa4895c1744c4ebc40bf1473b838
mc_ondros <popandrei2005ab@gmail.com>
2025-03-06 10:59:16 +0200

    first commit

---
+++ board.c
@@ -0,0 +1,44 @@
+#include <wchar.h>
+#include <locale.h>
+#include <stdlib.h>
+
+#define white_king   0x2654 // ♔
+#define white_queen  0x2655 // ♕
+#define white_rook   0x2656 // ♖
+#define white_bishop 0x2657 // ♗
+#define white_knight 0x2658 // ♘
+#define white_pawn   0x2659 // ♙
+#define black_king   0x265A // ♚
+#define black_queen  0x265B // ♛
+#define black_rook   0x265C // ♜
+#define black_bishop 0x265D // ♝
+#define black_knight 0x265E // ♞
+#define black_pawn   0x265F // ♟
+
+void createBoard(){
+    wchar_t board[8][8];
+    for(int i = 0; i < 8; i++){
+      for(int j = 0; j < 8; j++){
+          if (i == 0) {
+              if (j == 0 || j == 7) board[i][j] = white_rook;
+              if (j == 1 || j == 6) board[i][j] = white_knight;
+              if (j == 2 || j == 5) board[i][j] = white_bishop;
+              if (j == 3) board[i][j] = white_queen;
+              if (j == 4) board[i][j] = white_king;
+          }
+
+          if (i == 1) board[i][j] = white_pawn;
+
+          if (i > 1 && i < 6) board[i][j] = 0;
+
+          if (i == 6) board[i][j] = black_pawn;
+          if (i == 7) {
+              if(j == 0 || j == 7) board[i][j] = black_rook;
+              if(j == 1 || j == 6) board[i][j] = black_knight;
+              if(j == 2 || j == 5) board[i][j] = black_bishop;
+              if(j == 3) board[i][j] = black_queen;
+              if(j == 4) board[i][j] = black_king;
+          }
+      }
+      }
+    }
