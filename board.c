d886b64802675a697c60754fbcc5232bc685059b
mc_ondros <popandrei2005ab@gmail.com>
2025-03-31 12:17:45 +0300

    Update chess game implementation: adjust CMake settings, enhance board initialization, and add print functionality

--- board.c
+++ board.c
@@ -1,44 +1,49 @@
 #include <wchar.h>
-#include <locale.h>
-#include <stdlib.h>
+#include <stdio.h>
+#include "chess.h"
 
-#define white_king   0x2654 // ♔
-#define white_queen  0x2655 // ♕
-#define white_rook   0x2656 // ♖
-#define white_bishop 0x2657 // ♗
-#define white_knight 0x2658 // ♘
-#define white_pawn   0x2659 // ♙
-#define black_king   0x265A // ♚
-#define black_queen  0x265B // ♛
-#define black_rook   0x265C // ♜
-#define black_bishop 0x265D // ♝
-#define black_knight 0x265E // ♞
-#define black_pawn   0x265F // ♟
+// Global board declaration (defined in chess.h as extern)
+wchar_t board[8][8];
 
-void createBoard(){
-    wchar_t board[8][8];
-    for(int i = 0; i < 8; i++){
-      for(int j = 0; j < 8; j++){
-          if (i == 0) {
-              if (j == 0 || j == 7) board[i][j] = white_rook;
-              if (j == 1 || j == 6) board[i][j] = white_knight;
-              if (j == 2 || j == 5) board[i][j] = white_bishop;
-              if (j == 3) board[i][j] = white_queen;
-              if (j == 4) board[i][j] = white_king;
-          }
+void createBoard() {
+    for(int i = 0; i < 8; i++) {
+        for(int j = 0; j < 8; j++) {
+            if (i == 0) {
+                if (j == 0 || j == 7) board[i][j] = white_rook;
+                if (j == 1 || j == 6) board[i][j] = white_knight;
+                if (j == 2 || j == 5) board[i][j] = white_bishop;
+                if (j == 3) board[i][j] = white_queen;
+                if (j == 4) board[i][j] = white_king;
+            }
 
-          if (i == 1) board[i][j] = white_pawn;
+            if (i == 1) board[i][j] = white_pawn;
 
-          if (i > 1 && i < 6) board[i][j] = 0;
+            if (i > 1 && i < 6) board[i][j] = 0;
 
-          if (i == 6) board[i][j] = black_pawn;
-          if (i == 7) {
-              if(j == 0 || j == 7) board[i][j] = black_rook;
-              if(j == 1 || j == 6) board[i][j] = black_knight;
-              if(j == 2 || j == 5) board[i][j] = black_bishop;
-              if(j == 3) board[i][j] = black_queen;
-              if(j == 4) board[i][j] = black_king;
-          }
-      }
-      }
+            if (i == 6) board[i][j] = black_pawn;
+            if (i == 7) {
+                if(j == 0 || j == 7) board[i][j] = black_rook;
+                if(j == 1 || j == 6) board[i][j] = black_knight;
+                if(j == 2 || j == 5) board[i][j] = black_bishop;
+                if(j == 3) board[i][j] = black_queen;
+                if(j == 4) board[i][j] = black_king;
+            }
+        }
     }
+}
+
+void printBoard() {
+    wprintf(L"  a b c d e f g h\n");
+    wprintf(L"  ---------------\n");
+    for (int i = 7; i >= 0; i--) {
+        wprintf(L"%d|", i + 1);
+        for (int j = 0; j < 8; j++) {
+            if (board[i][j] == 0) {
+                wprintf(L"  ");
+            } else {
+                wprintf(L"%lc ", board[i][j]);
+            }
+        }
+        wprintf(L"\n");
+    }
+}
--- main.c
+++ main.c
@@ -4,62 +4,49 @@
 #include <locale.h>
 #include <fcntl.h>
 #include <io.h>
-#define white_king   0x2654 // ♔
-#define white_queen  0x2655 // ♕
-#define white_rook   0x2656 // ♖
-#define white_bishop 0x2657 // ♗
-#define white_knight 0x2658 // ♘
-#define white_pawn   0x2659 // ♙
-#define black_king   0x265A // ♚
-#define black_queen  0x265B // ♛
-#define black_rook   0x265C // ♜
-#define black_bishop 0x265D // ♝
-#define black_knight 0x265E // ♞
-#define black_pawn   0x265F // ♟
-
-wchar_t board[8][8];
-
-void createBoard() {
-    for (int i = 0; i < 8; i++) {
-        for (int j = 0; j < 8; j++) {
-            if (i == 0) {
-                if (j == 0 || j == 7) board[i][j] = white_rook;
-                if (j == 1 || j == 6) board[i][j] = white_knight;
-                if (j == 2 || j == 5) board[i][j] = white_bishop;
-                if (j == 3) board[i][j] = white_queen;
-                if (j == 4) board[i][j] = white_king;
-            }
-
-            if (i == 1) board[i][j] = white_pawn;
-
-            if (i > 1 && i < 6) board[i][j] = 0;
-
-            if (i == 6) board[i][j] = black_pawn;
-            if (i == 7) {
-                if (j == 0 || j == 7) board[i][j] = black_rook;
-                if (j == 1 || j == 6) board[i][j] = black_knight;
-                if (j == 2 || j == 5) board[i][j] = black_bishop;
-                if (j == 3) board[i][j] = black_queen;
-                if (j == 4) board[i][j] = black_king;
-            }
-        }
-    }
-}
-
-void printBoard() {
-    for (int i = 0; i < 8; i++) {
-        for (int j = 0; j < 8; j++) {
-            wprintf(L"%lc ", board[i][j]);
-        }
-        wprintf(L"\n");
-    }
-}
+#include "chess.h"
 
 int main(void) {
     setlocale(LC_ALL, "");
     _setmode(_fileno(stdout), _O_U16TEXT);  // Enable Unicode output in Windows
 
-createBoard();
+    createBoard();
     printBoard();
+    
+    // Convert back to normal output for input handling
+    _setmode(_fileno(stdout), _O_TEXT);
+    
+    int fromRow, fromCol, toRow, toCol;
+    int playerTurn = 1; // 1 for white, 0 for black
+    
+    while (1) {
+        printf("\n%s's turn\n", playerTurn ? "White" : "Black");
+        
+        if (!readMove(&fromRow, &fromCol, &toRow, &toCol)) {
+            printf("Invalid input format. Use format like e2e4.\n");
+            continue;
+        }
+        
+        // Check if player is moving their own piece
+        wchar_t piece = board[fromRow][fromCol];
+        if ((playerTurn && !isPieceWhite(piece)) || (!playerTurn && !isPieceBlack(piece))) {
+            printf("You can only move your own pieces.\n");
+            continue;
+        }
+        
+        if (!isValidMove(fromRow, fromCol, toRow, toCol)) {
+            printf("Invalid move for this piece.\n");
+            continue;
+        }
+        
+        executeMove(fromRow, fromCol, toRow, toCol);
+        playerTurn = !playerTurn; // Switch turns
+        
+        // Switch back to Unicode for board display
+        _setmode(_fileno(stdout), _O_U16TEXT);
+        printBoard();
+        _setmode(_fileno(stdout), _O_TEXT);
+    }
+    
     return 0;
 }
